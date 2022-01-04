a2877b5822893e1dee748c7691f73d508f51c08a
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2022-01-03 21:17:31 -0500

    deploying

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,7 +2,7 @@ const db = require("../models/Workout.js");
 
 
 module.exports = function (app) {
-  app.get("/api/Workouts", (req, res) => {
+  app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then((workoutdb) => {
         res.json(workoutdb);
@@ -11,7 +11,7 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
-  app.get("/api/Workouts/range", ({}, res) => {
+  app.get("/api/workouts/range", ({}, res) => {
     db.Workout.find({})
       .then((workoutdb) => {
         res.json(workoutdb);
@@ -20,7 +20,7 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
-  app.post("/api/Workouts", (req, res) => {
+  app.post("/api/workouts", (req, res) => {
     const workout = body;
     db.Workout.save(workout, (err, saved))
       .then((saved) => {
@@ -30,7 +30,7 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
-  app.put("/api/Workouts/:id", (req, res) => {
+  app.put("/api/workouts/:id", (req, res) => {
     db.Workout.updateOne(
       { _id: mongojs.ObjectId(params.id) },
       { $set: { exercises: req.body } }
