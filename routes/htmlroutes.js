a57d8e7befc74ecbf9463f587a68920308301fee
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2022-01-03 21:18:38 -0500

    deploying

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   });
 
   app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/Exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
   app.get("/stats", function (req, res) {
