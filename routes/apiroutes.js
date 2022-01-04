46c9e4de1fd26efcd5d97f850d1004edec7eccee
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2022-01-03 21:16:13 -0500

    deploying

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,8 @@
-const db = require("../models/workout.js");
+const db = require("../models/Workout.js");
 
 
 module.exports = function (app) {
-  app.get("/api/workouts", (req, res) => {
+  app.get("/api/Workouts", (req, res) => {
     db.Workout.find({})
       .then((workoutdb) => {
         res.json(workoutdb);
@@ -11,7 +11,7 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
-  app.get("/api/workouts/range", ({}, res) => {
+  app.get("/api/Workouts/range", ({}, res) => {
     db.Workout.find({})
       .then((workoutdb) => {
         res.json(workoutdb);
@@ -20,7 +20,7 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
-  app.post("/api/workouts", (req, res) => {
+  app.post("/api/Workouts", (req, res) => {
     const workout = body;
     db.Workout.save(workout, (err, saved))
       .then((saved) => {
@@ -30,7 +30,7 @@ module.exports = function (app) {
         res.status(500).json(err);
       });
   });
-  app.put("/api/workouts/:id", (req, res) => {
+  app.put("/api/Workouts/:id", (req, res) => {
     db.Workout.updateOne(
       { _id: mongojs.ObjectId(params.id) },
       { $set: { exercises: req.body } }
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   });
 
   app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/Exercise.html"));
   });
 
   app.get("/stats", function (req, res) {
